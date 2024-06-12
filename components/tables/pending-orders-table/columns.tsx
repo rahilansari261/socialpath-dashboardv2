f5a6d55ca5781c545e770c5e5151eb5c0117ada8
Rahil Ansari <rahilansari261@gmail.com>
2024-06-12 16:23:49 +0530

    fixes 4

--- components/tables/pending-orders-table/columns.tsx
+++ components/tables/pending-orders-table/columns.tsx
@@ -3,7 +3,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { CellAction } from "./cell-action";
 
 // import { Checkbox } from "@/components/ui/checkbox";
-import { Badge } from "@/components/ui/badge";
+// import { Badge } from "@/components/ui/badge";
 import { Order } from "@prisma/client";
 
 export const columns: ColumnDef<Order>[] = [
@@ -49,23 +49,23 @@ export const columns: ColumnDef<Order>[] = [
     header: "PAYMENT ID",
   },
 
-  {
-    accessorKey: "status",
-    header: "STATUS",
-    cell: ({ row }) => {
-      // Render a Badge component with the status value
-      const variant =
-        row.original.status === "pending"
-          ? "default"
-          : row.original.status === "rejected"
-          ? "destructive"
-          : row.original.status === "accepted"
-          ? "secondary"
-          : "secondary";
+  // {
+  //   accessorKey: "status",
+  //   header: "STATUS",
+  //   cell: ({ row }) => {
+  //     // Render a Badge component with the status value
+  //     const variant =
+  //       row.original.status === "pending"
+  //         ? "default"
+  //         : row.original.status === "rejected"
+  //         ? "destructive"
+  //         : row.original.status === "accepted"
+  //         ? "secondary"
+  //         : "secondary";
 
-      return <Badge variant={variant}>{row.original.status.charAt(0).toUpperCase() + row.original.status.slice(1)}</Badge>;
-    },
-  },
+  //     return <Badge variant={variant}>{row.original.status.charAt(0).toUpperCase() + row.original.status.slice(1)}</Badge>;
+  //   },
+  // },
 
   {
     id: "actions",
